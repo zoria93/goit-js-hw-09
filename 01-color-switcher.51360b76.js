dfec41e3b79ea6c24c52be99641451ab58f4e660
zoria93 <110671913+zoria93@users.noreply.github.com>
2023-01-23 20:18:12 +0000

    Deploying to gh-pages from @ zoria93/goit-js-hw-09@3ff6a7df945359f00df990312841343fb8fd4689 🚀

---
+++ 01-color-switcher.51360b76.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.51360b76.js.map
--- 01-color-switcher.a5828160.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),r=null;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),r=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.setAttribute("disabled","false"),clearInterval(r)}))}();
-//# sourceMappingURL=01-color-switcher.a5828160.js.map
